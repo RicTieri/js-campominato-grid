dfcd13f99028f7878b8f26ef6574c7caa6e317ce
RicTieri <tieri.riccardo@gmail.com>
2023-11-21 17:37:56 +0100

    added grid

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 
 const gameBox = document.getElementById('game_box');
-const playButton = document.querySelector('button')
+const playButton = document.querySelector('button');
 
-gameBox.appendChild(getDivWithClassAndText('game_card', 'ciao'));
-
-
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+playButton.addEventListener('click',function (){
+  gameBox.innerHTML = ''
+  // Ogni cella ha un numero progressivo, da 1 a 100.
+  for (let i = 1; i <= 100; i++) {
+    gameBox.appendChild(getDivWithClassAndText('game_card', i));
+  }
+})
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
+divElement.addEventListener('click')
 
 
 
