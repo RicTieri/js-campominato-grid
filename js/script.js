212409f9f7b6e0b07e17c9099332bf38d768a95d
RicTieri <tieri.riccardo@gmail.com>
2023-11-21 18:14:12 +0100

    added difficult select

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ const gameBox = document.getElementById('game_box');
 const playButton = document.querySelector('button');
 
 playButton.addEventListener('click',function (){
+  let gameLevel = document.querySelector('select');
+  let totalCard = gameLevel.value;
   gameBox.innerHTML = ''
   // Ogni cella ha un numero progressivo, da 1 a 100.
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= totalCard; i++) {
     const divSquare = getDivWithClassAndText('game_card', i);
     gameBox.appendChild(divSquare);
+    divSquare.style.width = `calc(100% / ${Math.sqrt(totalCard)})`
+    divSquare.style.height = `calc(100% / ${Math.sqrt(totalCard)})`
     divSquare.addEventListener('click', function(){
       divSquare.classList.add('clicked');
       console.log(i)
